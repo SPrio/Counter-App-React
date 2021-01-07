2551b9eb9a4ad20c80bd5906de9617e2d3678c3f
SPrio <mahantasupriyo@gmail.com>
2021-01-07 13:35:22 +0530

    raising and handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,10 +11,8 @@ class Counter extends Component {
   };
 
   render() {
-    console.log(this.props);
     return (
       <div>
-        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement({ id: 1 })}
@@ -22,6 +20,7 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
   }
